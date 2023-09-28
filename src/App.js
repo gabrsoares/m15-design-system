0958993945d010a0a1d352fbf86b6a15ef1f3b89
Gabriel Soares <gabrsoares@outlook.com>
2023-09-28 15:17:07 -0300

    fix & deploy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
-import { Button } from './ds';
-import { Select } from './ds';
 
 function App() {
   return (
     <div className="App">
-      <h1>Testando</h1>
-      <Button variation="secondary">Click me</Button>
-      <Select variation="secondary" fSize="big" size="big" labelText="Testando">
-        <option value="ola">ola</option>
-      </Select>
+      
     </div>
   );
 }
